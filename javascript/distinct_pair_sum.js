52fb8e9445c738e68d9603d412067fa2308f4347
Rupa <rupa211984@gmail.com>
2020-11-10 22:37:04 -0800

    Readme

---
+++ javascript/distinct_pair_sum.js
@@ -0,0 +1,19 @@
+function distinctPairSum(arr, k) {
+  // type your code here
+}
+
+if (require.main === module) {
+  // add your own tests in here
+  console.log("Expecting: 3");
+  console.log(distinctPairSum([2, 1, 3, 3, 2]));
+
+  console.log("");
+
+  console.log("Expecting: -1");
+  console.log(distinctPairSum([1, 2, 3, 4]));
+}
+
+module.exports = distinctPairSum;
+
+// Please add your pseudocode to this file
+// And a written explanation of your solution
