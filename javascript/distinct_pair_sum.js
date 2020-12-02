016bdc7c3bce50b8ac1e7efef8271cb5d3303397
Rupa <rupa211984@gmail.com>
2020-12-02 12:40:48 -0800

    Add arrows

--- javascript/distinct_pair_sum.js
+++ javascript/distinct_pair_sum.js
@@ -5,12 +5,12 @@ function distinctPairSum(arr, k) {
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [[1, 1], [2, 0]]");
-  console.log(distinctPairSum([0, 1, 1, 2, 0, 1, 1], 2));
+  console.log("=>", distinctPairSum([0, 1, 1, 2, 0, 1, 1], 2));
 
   console.log("");
 
   console.log("Expecting: [[2, 8]]");
-  console.log(distinctPairSum([3, 4, 2, 1, 5, 2, 8, 2], 10));
+  console.log("=>", distinctPairSum([3, 4, 2, 1, 5, 2, 8, 2], 10));
 }
 
 module.exports = distinctPairSum;
--- ruby/distinct_pair_sum.rb
+++ ruby/distinct_pair_sum.rb
@@ -4,11 +4,13 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: [[1, 1], [2, 0]]"
+  print "=> "
   print distinct_pair_sum([0, 1, 1, 2, 0, 1, 1], 2)
 
   puts
 
   puts "Expecting: [[2, 8]]"
+  print "=> "  
   print distinct_pair_sum([3, 4, 2, 1, 5, 2, 8, 2], 10)
 
   # Don't forget to add your own!
